e6b71da5531d39ab2188a892933a1ddff49978ee
ksk9905007-cloud <ksk9905007@gmail.com>
2026-04-05 16:08:32 +0900

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
-# Install python dependencies
-pip install -r requirements.txt && playwright install chromium
-
-# Install python playwright binaries and their dependencies
+# 파이썬 의존성 설치
+pip install -r requirements.txt
+# 브라우저 파일만 설치 (시스템 라이브러리 설치 시도인 install-deps 제거)
 playwright install chromium
-playwright install-deps chromium
